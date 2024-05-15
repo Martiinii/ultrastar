fb71fbcfa8e2ba633ad590ea05aa93ae18548bf7
Martiinii <me.martiinii@gmail.com>
2024-05-15 22:58:38 +0200

    Parse youtube links from comments and not from page

--- packages/ultrastar-api/src/lib/parser/parseYoutubeLinks.ts
+++ packages/ultrastar-api/src/lib/parser/parseYoutubeLinks.ts
@@ -1,26 +1,33 @@
+import { parseComments } from "./parseComments";
+
 export const parseYoutubeLinks = (html: string) => {
-  const links = [
-    ...html.matchAll(
-      /<td>(\d+\.\d+\.\d+) - (\d+:\d+).*$\s*.*?src="(.*?(?=youtu\.?be).*?)"/gm
-    ),
-  ]
-    .map(parseYoutubeRegexMatch)
+  const comments = parseComments(html)
+    .map((c) =>
+      c.match(
+        /<td>(\d+\.\d+\.\d+) - (\d+:\d+)[\s\S]*src="(.*?(?=youtu\.?be).*?)"/m
+      )
+    )
+    .map(parseYoutubeLinkFromComment)
     .filter(Boolean);
 
-  return links;
+  return comments;
 };
 
-const parseYoutubeRegexMatch = (r: RegExpExecArray) => {
+const parseYoutubeLinkFromComment = (r: RegExpMatchArray | null) => {
   const dateStr = r?.[1];
   const timeStr = r?.[2];
-  const link = r?.[3];
+  const linkRaw = r?.[3];
 
-  if (!dateStr || !timeStr || !link) return null;
+  if (!dateStr || !timeStr || !linkRaw) return null;
 
   const [day, month, year] = dateStr.split(".") as [string, string, string];
   const [hour, minute] = timeStr.split(":") as [string, string];
 
+  console.log({ day, month, year, hour, minute });
+
   const date = new Date(`${month}.${day}.${year} ${hour}:${minute}`);
+
+  const link = linkRaw.split("/").pop();
 
   return {
     createdAt: date,
