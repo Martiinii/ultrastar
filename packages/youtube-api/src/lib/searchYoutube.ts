807e1f2f4f31672d416b34eb39099bc6446bb658
Martiinii <me.martiinii@gmail.com>
2024-05-17 23:57:10 +0200

    Create function to search songs on youtube

---
+++ packages/youtube-api/src/lib/searchYoutube.ts
@@ -0,0 +1,11 @@
+import { $ } from "bun";
+
+export const searchYoutube = async (search: string) => {
+  const response = (
+    await $`yt-dlp --match-filters 'original_url!*=/shorts/'   "ytsearch5:${search}" --flat-playlist -j --no-simulate`
+  ).text();
+
+  return JSON.parse(
+    `[${response.split("\n").filter(Boolean).join(",")}]`
+  ) as YoutubeVideo[];
+};
