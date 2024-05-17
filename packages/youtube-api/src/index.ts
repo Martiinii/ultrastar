baadb6fb79681b6de9f0acfd5cedc44f2ffa4b21
Martiinii <me.martiinii@gmail.com>
2024-05-17 23:57:16 +0200

    Testing

--- packages/youtube-api/src/index.ts
+++ packages/youtube-api/src/index.ts
@@ -1,10 +1,12 @@
-import { downloadYoutubeVideo } from "./lib/downloadYoutubeVideo";
+import { searchYoutube } from "./lib/searchYoutube";
 
 const main = async () => {
-  const link = "-tVWt2RDsoM";
-  const path = "video.mp4";
+  //   const link = "-tVWt2RDsoM";
+  //   const path = "video.mp4";
+  //   await downloadYoutubeVideo(link, path);
 
-  await downloadYoutubeVideo(link, path);
+  const res = await searchYoutube("Dua Lipa - Illusion");
+  Bun.write("data.json", JSON.stringify(res));
 };
 
 main();
