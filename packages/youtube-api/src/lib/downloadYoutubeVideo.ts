2c3aea622af8fc18666d1e7971b92f2672f6a837
Martiinii <me.martiinii@gmail.com>
2024-05-30 23:32:46 +0200

    feat: add function to download from video

--- packages/youtube-api/src/lib/downloadYoutubeVideo.ts
+++ packages/youtube-api/src/lib/downloadYoutubeVideo.ts
@@ -1,5 +1,15 @@
 import { $ } from "bun";
 
-export const downloadYoutubeVideo = async (link: string, path: string) => {
+export const downloadYoutubeVideoFromLink = async (
+  link: string,
+  path: string
+) => {
   await $`yt-dlp -S "ext,res:1080" -o '${path}' -- ${link}`.quiet();
+};
+
+export const downloadYoutubeVideo = async (
+  video: YoutubeVideo,
+  path: string
+) => {
+  await downloadYoutubeVideoFromLink(video.id, path);
 };
