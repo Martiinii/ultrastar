dd50f572dfe3b6a8725d9ba2232724525ef65383
Martiinii <me.martiinii@gmail.com>
2024-05-30 23:32:26 +0200

    fix: remove output from bun shell

--- packages/youtube-api/src/lib/searchYoutube.ts
+++ packages/youtube-api/src/lib/searchYoutube.ts
@@ -1,9 +1,8 @@
 import { $ } from "bun";
 
 export const searchYoutube = async (search: string) => {
-  const response = (
-    await $`yt-dlp --match-filters 'original_url!*=/shorts/'   "ytsearch5:${search}" --flat-playlist -j --no-simulate`
-  ).text();
+  const response =
+    await $`yt-dlp --match-filters 'original_url!*=/shorts/'   "ytsearch5:${search}" --flat-playlist -j --no-simulate`.text();
 
   return JSON.parse(
     `[${response.split("\n").filter(Boolean).join(",")}]`
