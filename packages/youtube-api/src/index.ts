a6ad8e05b4bd3499ff5fbb80898dbfbf179df0bc
Martiinii <me.martiinii@gmail.com>
2024-05-30 23:33:18 +0200

    chore: remove main used for tests, add export

--- packages/youtube-api/src/index.ts
+++ packages/youtube-api/src/index.ts
@@ -1,12 +1,3 @@
-import { searchYoutube } from "./lib/searchYoutube";
+/// <reference types="@ultrastar/types" />
 
-const main = async () => {
-  //   const link = "-tVWt2RDsoM";
-  //   const path = "video.mp4";
-  //   await downloadYoutubeVideo(link, path);
-
-  const res = await searchYoutube("Dua Lipa - Illusion");
-  console.log(res);
-};
-
-main();
+export * from "./lib";
