22d433378dcd501dc7a5fd087ab83f4398257097
Martiinii <me.martiinii@gmail.com>
2024-06-04 20:10:25 +0200

    chore: remove actions file used for testing

--- apps/web/app/actions.ts
+++
@@ -1,7 +0,0 @@
-"use server";
-
-import { api } from "@ultrastar/libs";
-export const handleButtonClick = async () => {
-  const response = await api.songs({ id: 28992 }).download.get();
-  console.log(response);
-};
