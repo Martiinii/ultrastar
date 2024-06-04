dce200bb95a4d4eb89b8ae564b01f3de3e71f659
Martiinii <me.martiinii@gmail.com>
2024-06-04 20:14:30 +0200

    feat: add logger

--- apps/api/src/plugins/setup.ts
+++ apps/api/src/plugins/setup.ts
@@ -1,3 +1,11 @@
 import Elysia from "elysia";
 
-export const setupPlugin = new Elysia({ name: "setup" });
+export const setupPlugin = new Elysia({ name: "setup" }).onBeforeHandle(
+  { as: "global" },
+  ({ request }) => {
+    const path = new URL(request.url).pathname;
+    const log = `${request.method} ${path}`;
+
+    console.log(log);
+  }
+);
