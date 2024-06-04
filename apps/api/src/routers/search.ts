a0e053bfbb8ae4b163ea4855c5a06e4476e0fe7e
Martiinii <me.martiinii@gmail.com>
2024-06-04 20:14:48 +0200

    feat: add /:page and /count routes

--- apps/api/src/routers/search.ts
+++ apps/api/src/routers/search.ts
@@ -1,5 +1,9 @@
 import { getLyricsById } from "@ultrastar/ultrastar-api/src/lib/data";
+import { count } from "drizzle-orm";
 import Elysia, { t } from "elysia";
+import { db } from "../db";
+import { getSongs } from "../db/query/getSongs";
+import { songTable } from "../db/schema/songs";
 import { setupPlugin } from "../plugins/setup";
 
 export const searchRouter = new Elysia({
@@ -8,6 +12,11 @@ export const searchRouter = new Elysia({
   tags: ["Search"],
 })
   .use(setupPlugin)
+  .get("/:page", async ({ params: { page } }) => getSongs(page), {
+    params: t.Object({
+      page: t.Numeric(),
+    }),
+  })
   .get(
     "/id/:id",
     ({ params: { id } }) => {
@@ -19,4 +28,8 @@ export const searchRouter = new Elysia({
       }),
     }
   )
+  .get("/count", () => {
+    const res = db.select({ count: count() }).from(songTable).get();
+    return res?.count ?? 0;
+  })
   .get("/filters/:filter", "");
