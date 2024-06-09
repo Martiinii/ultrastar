c19b17ed92abcbcc2685e6b11301268d03cf4cdc
Martiinii <me.martiinii@gmail.com>
2024-06-09 20:11:25 +0200

    feat: wrap in forwardRef

--- apps/web/components/navbar/searchButton.tsx
+++ apps/web/components/navbar/searchButton.tsx
@@ -1,12 +1,19 @@
-import { Button } from "@ui/components/button";
+import { Button, type ButtonProps } from "@ui/components/button";
+import React from "react";
 
-export const SearchButton = () => {
-  return (
-    <Button
-      variant={"outline"}
-      className="text-muted-foreground text-sm font-normal md:w-64 w-full justify-start whitespace-nowrap"
-    >
-      Search songs...
-    </Button>
-  );
-};
+export const SearchButton = React.forwardRef<HTMLButtonElement, ButtonProps>(
+  (props, ref) => {
+    return (
+      <Button
+        variant={"outline"}
+        className="text-muted-foreground text-sm font-normal md:w-64 w-full justify-start whitespace-nowrap"
+        {...props}
+        ref={ref}
+      >
+        Search songs...
+      </Button>
+    );
+  }
+);
+
+SearchButton.displayName = "Search Button";
