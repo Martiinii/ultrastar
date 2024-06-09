2b3d74036e90c819eec3fa9cfdf3a4feefef5626
Martiinii <me.martiinii@gmail.com>
2024-06-09 20:14:57 +0200

    feat: add searching

--- apps/web/components/modals/searchSongModal.tsx
+++ apps/web/components/modals/searchSongModal.tsx
@@ -1,18 +1,43 @@
-import { Button } from "@ui/components/button";
+"use client";
+
+import { useMergeSearchParams } from "@/hooks/use-merge-search-params";
+import { useRouter } from "next/navigation";
+import { useState } from "react";
+import {
+  SearchSongsForm,
+  type SearchSongsFormSchema,
+} from "../forms/searchSongsForm";
 import { SearchButton } from "../navbar/searchButton";
 import { ResponsiveDialog } from "../responsiveDialog";
 
 export const SearchSongModal = () => {
+  const { createQueryParams } = useMergeSearchParams();
+  const router = useRouter();
+
+  const [isOpen, setIsOpen] = useState(false);
+
+  const handleSearch = ({ search, languages }: SearchSongsFormSchema) => {
+    setIsOpen(false);
+
+    const queryParams = createQueryParams(
+      ["search", search],
+      ["language", languages]
+    );
+    queryParams.delete("page");
+    router.push("/?" + queryParams.toString());
+  };
+
   return (
     <ResponsiveDialog
+      open={isOpen}
+      onOpenChange={setIsOpen}
       trigger={<SearchButton />}
       header={{
-        title: "Responsive dialog test",
-        description:
-          "This is a very long description to desribe the functionality of this very component",
+        title: "Search for songs",
+        description: "Search and filter based on the criteria provided",
       }}
     >
-      <Button>Hey!</Button>
+      <SearchSongsForm onSubmit={handleSearch} />
     </ResponsiveDialog>
   );
 };
