beca03e830afc94fe0f540aae1e78ff45d3c2d3b
Martiinii <me.martiinii@gmail.com>
2024-06-09 20:05:02 +0200

    fix: if request is not ok, return null

--- packages/ultrastar-api/src/lib/data/image.ts
+++ packages/ultrastar-api/src/lib/data/image.ts
@@ -3,6 +3,8 @@ import config from "../../config.json";
 export const getCoverImage = async (id: number) => {
   const url = `${config.apiUrl}/data/cover/${id}.jpg`;
   const response = await fetch(url);
+  if (!response.ok) return null;
+
   const data = await response.arrayBuffer();
   return new Uint8Array(data);
 };
